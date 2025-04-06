d9d42ac4c60e5563e46f5082635d7bbdfacd038e
赢政 <ynkyght@outlook.com>
2025-04-06 20:38:11 +0800

    完成对stm32f103c8t6的安富莱例程移植已经完成led,key,usart,timer有部分瑕疵

---
+++ 01/stm32f1_basic/MDK-ARM/stm32f1_basic/list.d
@@ -0,0 +1,12 @@
+stm32f1_basic\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/list.c
+stm32f1_basic\list.o: D:\keil\Mdk\ARM\ARMCC\Bin\..\include\stdlib.h
+stm32f1_basic\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+stm32f1_basic\list.o: D:\keil\Mdk\ARM\ARMCC\Bin\..\include\stddef.h
+stm32f1_basic\list.o: D:\keil\Mdk\ARM\ARMCC\Bin\..\include\stdint.h
+stm32f1_basic\list.o: ../Core/Inc/FreeRTOSConfig.h
+stm32f1_basic\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+stm32f1_basic\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+stm32f1_basic\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+stm32f1_basic\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM3/portmacro.h
+stm32f1_basic\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+stm32f1_basic\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ 01/stm32f1_basic/MDK-ARM/stm32f1_basic/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+stm32f1_basic\startup_stm32f103xb.o: startup_stm32f103xb.s
